69526ec40b729281bf1c24191350c7aec77f744e
Pavel Chigrin <pavel.chigrin@choobs.com>
2021-06-02 00:28:47 +0300

    fix validation pipe
    form errors now respect multi level objects

--- src/pipe/validation.pipe.ts
+++ src/pipe/validation.pipe.ts
@@ -3,14 +3,24 @@ import { ValidationError } from 'class-validator';
 import { InvalidFormException } from 'src/exeption/invalid-form.exception';
 
 export class ValidationPipeCustomException extends ValidationPipe {
-  public createExceptionFactory() {
-    return (errors?: ValidationError[]) => {
-      const messages = errors.map((error) => {
+  private walk(errors: ValidationError[], path = '') {
+    return errors.map((error) => {
+      const pathNew = path + (path ? '.' : '') + error.property;
+
+      if (error.constraints) {
         return {
-          path: error.property,
+          path: pathNew,
           constraints: error.constraints,
         };
-      });
+      }
+
+      return this.walk(error.children, pathNew);
+    });
+  }
+
+  public createExceptionFactory() {
+    return (errors?: ValidationError[]) => {
+      const messages = this.walk(errors);
 
       if (messages.length) {
         throw new InvalidFormException(messages);
