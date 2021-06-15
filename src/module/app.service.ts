73a277c4ce5b9e049accd04674e931640db3ecc1
Pavel Chigrin <pavel.chigrin@choobs.com>
2021-06-15 22:13:20 +0300

    bump version

--- src/module/app.service.ts
+++ src/module/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getApiLinks(): string {
-    return '<a href="/api/v0">API V0</a><a href="/api/v1">API V1</a>';
+    return '<a href="/api/v1">API V1</a>';
   }
 }
