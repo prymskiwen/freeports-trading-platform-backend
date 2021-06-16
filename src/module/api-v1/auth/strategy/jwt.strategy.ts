b39259a70dd2f61e3e8137e2c2787c25f66a907f
Pavel Chigrin <pavel.chigrin@choobs.com>
2021-06-17 00:21:05 +0300

    add suspended user checks
    check at refresh token
    check at jwt and jwt2fa strategies

--- src/module/api-v1/auth/strategy/jwt.strategy.ts
+++ src/module/api-v1/auth/strategy/jwt.strategy.ts
@@ -4,6 +4,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import authenticationConfig from 'src/config/auth.config';
 import { InvalidCredentialsException } from 'src/exeption/invalid-credentials.exception';
+import { SuspendedUserException } from 'src/exeption/suspended-user.exception';
 import { WrongTokenException } from 'src/exeption/wrong-token.exception';
 import { UserDocument } from 'src/schema/user/user.schema';
 import { UserService } from '../../user/user.service';
@@ -32,6 +33,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
     if (!user) {
       throw new InvalidCredentialsException();
+    }
+
+    if (user.suspended) {
+      throw new SuspendedUserException();
     }
 
     return user;
