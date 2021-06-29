652b705e1f2e34d34d0548294116801411651b06
Pavel Chigrin <pavel.chigrin@choobs.com>
2021-06-29 13:49:08 +0300

    fix missed operation module definition

--- src/module/api-v1/api.v1.module.ts
+++ src/module/api-v1/api.v1.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { AccountModule } from './account/account.module';
-import { OperationModule } from './accountOperation/operation.module';
 import { DeskModule } from './desk/desk.module';
 import { OrganizationModule } from './organization/organization.module';
 import { RoleModule } from './role/role.module';
 import { UserModule } from './user/user.module';
 import { InvestorModule } from './investor/investor.module';
+import { OperationModule } from './account-operation/operation.module';
 
 @Module({
   imports: [
