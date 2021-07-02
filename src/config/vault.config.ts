025ccf06abee5296e7aeea22ae56d967d6e2bbb7
Muhammed <aapozaid@gmail.com>
2021-07-02 02:59:13 +0200

    Vault api proxy

--- src/config/vault.config.ts
+++ src/config/vault.config.ts
@@ -2,6 +2,4 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('vault', () => ({
   baseURL: process.env.VAULT_API_BASEURL || 'https://apif.hubsecurity.io',
-  privateKey: process.env.VAULT_API_PRIVATE_KEY || 'private.key',
-  publicKey: process.env.VAULT_API_PUBLIC_KEY || 'public.key',
 }));
