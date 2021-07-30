61932663344efd1e794b0103abeac89f02f6823d
itapple <itapple.tech000@gmail.com>
2021-07-31 00:18:08 +0800

    Update smtp configure

--- src/module/api-v1/mail/mail.service.ts
+++ src/module/api-v1/mail/mail.service.ts
@@ -13,7 +13,8 @@ export class MailService {
     const smtpConfig = {
       host: process.env.MAIL_HOST,
       port: process.env.MAIL_PORT,
-      secure: true,
+      secure: process.env.MAIL_SECURE === 'true',
+      requireTLS: process.env.MAIL_REQUIRETLS === 'true',
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD,
