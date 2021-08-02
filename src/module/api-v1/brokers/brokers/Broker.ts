4fa79787914a5ae14f1c2f481e906119d9f69bf9
Muhammed <aapozaid@gmail.com>
2021-08-02 07:08:36 +0200

    brokers initials

---
+++ src/module/api-v1/brokers/brokers/Broker.ts
@@ -0,0 +1,105 @@
+import axios, { AxiosInstance } from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+// import { io, Socket } from 'socket.io-client';
+
+export interface OrderResponse {
+  order_id: string;
+  client_order_id: string;
+  quantity: string;
+  side: string;
+  instrument: string;
+  price: string;
+  executed_price: string;
+  executing_unit: string;
+  trades?: TradesEntity[] | null;
+  created: string;
+}
+export interface TradesEntity {
+  instrument: string;
+  trade_id: string;
+  origin: string;
+  rfq_id?: null;
+  created: string;
+  price: string;
+  quantity: string;
+  order: string;
+  side: string;
+  executing_unit: string;
+}
+export interface OrderResponse {
+  order_id: string;
+  client_order_id: string;
+  quantity: string;
+  side: string;
+  instrument: string;
+  price: string;
+  executed_price: string;
+  executing_unit: string;
+  trades?: TradesEntity[] | null;
+  created: string;
+}
+
+export enum Side {
+  'buy' = 'buy',
+  'sell' = 'sell',
+}
+
+export enum OrderType {
+  'FOK',
+  'MKT',
+}
+
+export class Broker {
+  name = 'B2C2';
+  BASE_URL = 'https://api.uat.b2c2.net';
+  SOCKET_BASE_URL = 'wss://socket.uat.b2c2.net/quotes';
+
+  accessToken = '';
+  //
+  axiosInstance: AxiosInstance;
+
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: this.BASE_URL,
+      headers: { Authorization: `Token ${this.accessToken}` },
+    });
+  }
+
+  rfq(
+    id: string,
+    instrument: string,
+    side: Side,
+    quantity: string,
+  ): Promise<any> {
+    return this.axiosInstance.post('/request_for_quote/', {
+      instrument: instrument,
+      side: side,
+      quantity: quantity,
+      client_rfq_id: id,
+    });
+  }
+
+  order(
+    id: string,
+    instrument: string,
+    side: Side,
+    quantity: string,
+    price: string,
+    validUntil: number,
+    executing_unit?: string,
+  ): Promise<OrderResponse> {
+    return this.axiosInstance.post('/order/', {
+      instrument: instrument,
+      side: side,
+      quantity: quantity,
+      client_rfq_id: id,
+      price,
+    });
+  }
+}
+
+const broker = new Broker();
+
+broker.rfq(uuidv4(), 'BTCUSD.SPOT', Side.buy, '1').then((response) => {
+  console.log('response ', response.data);
+});
